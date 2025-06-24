e2a66156fd4b9ae584f115f80f47b0a469e148bf
shawmeer <snasri898@gmail.com>
2025-06-24 15:16:58 +0545

    edited deploy.yml

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,6 @@
 output "instance_public_ip" {
   value = aws_instance.web_server.public_ip
 }
+output "domain_name" {
+  value = var.domain_name
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,7 @@ variable "key_name" {
   description = "Name of your existing AWS Key Pair"
   type        = string
 }
+variable "domain_name" {
+  description = "Domain name for the web server"
+  type        = string
+}
